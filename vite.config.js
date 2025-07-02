c0387e99185ff28dec7077e2522289eccd9bf526
Ganesh <ganeshp.py07@gmail.com>
2025-07-02 14:58:01 +0530

    modified for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  base:process.env.VITE_BASE_PATH || "/pawarfarm-web",
   server:{
     'port':3000
   }
